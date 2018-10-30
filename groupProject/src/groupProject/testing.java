5722891bc9175107c62f6cbba8d2a9a1b389629f
k <k@k.gsu.edu>
2018-10-30 19:32:41 -0400

    testing again

--- groupProject/src/groupProject/testing.java
+++ groupProject/src/groupProject/testing.java
@@ -3,9 +3,13 @@ package groupProject;
 public class testing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("testing");
 		System.out.println("hello");
+		
+		System.out.println("testing again commit");
+		
 	}
+	
 
 }
