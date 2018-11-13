982b217f6f443ebd0230605583e65f7119c236de
k <k@k.gsu.edu>
2018-11-13 18:34:16 -0500

    trying to add the JUnit

---
+++ groupProject/testsrc/groupProject/projectTest1Test.java
@@ -0,0 +1,29 @@
+package groupProject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class projectTest1Test {
+
+	@Test
+	void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testProjectTest1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStartStage() {
+		fail("Not yet implemented");
+	}
+
+}
