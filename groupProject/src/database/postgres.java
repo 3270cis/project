a31bc6bf34d54419373ded8fd03f4f179586f4b5
k <k@k>
2018-11-25 23:48:04 -0500

    update

--- groupProject/src/database/postgres.java
+++ groupProject/src/database/postgres.java
@@ -10,7 +10,9 @@ public class postgres {
 			Class.forName("org.postgresql.Driver");
 			
 			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/AirlineDatabase", "postgres", "passpass");
-			
+					
+			//"jdbc:postgresql://localhost:5432/AirlineDatabase", "postgres", "passpass"
+			//hafsah connectivity: "jdbc:postgresql://localhost:5432/hafsahqazi", "postgres", "admin"
 			System.out.println("Database connected");
 			
 			PreparedStatement statement = connection.prepareStatement("select from User");
@@ -18,14 +20,18 @@ public class postgres {
 			statement.executeQuery();
 			
 			
-			/*ResultSet resultSet = statement.executeQuery();
+			ResultSet resultSet = statement.executeQuery();
 			
 			while (resultSet.next())
 			      System.out.println(resultSet.getInt(1) + "\t" +
 			        resultSet.getString(2));
-		*/
+		
 			    // Close the connection
-			    //connection.close();
+			    connection.close();
+			
+			//KELLY, what does code line 23 to 28 do?
+			    //successful connection, but
+			    //gives error "The column index is out of range: 1, number of columns: 0."
 
 			
 		} catch(Exception ex) {
